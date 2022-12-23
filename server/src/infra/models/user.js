752158238132add860d26a36acbfcdf81ec5018a
eemr3 <eemr3@yahoo.com.br>
2022-12-23 12:11:06 -0300

    refactor: refactor model user

--- server/src/infra/models/user.js
+++ server/src/infra/models/user.js
@@ -4,22 +4,65 @@ module.exports = (sequelize, DataTypes) => {
     'User',
     {
       id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
         autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      phoneNumber: DataTypes.STRING,
-      mobile: DataTypes.STRING,
-      departament: DataTypes.STRING,
-      verificationCode: DataTypes.STRING,
-      emailChecked: DataTypes.BOOLEAN,
-      cashforceAdm: DataTypes.BOOLEAN,
+      name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: 'email',
+      },
+      phoneNumber: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      mobile: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      departament: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      verificationCode: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+      emailChecked: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      cashforceAdm: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: 0,
+      },
     },
     {
       sequelize,
       tableName: 'users',
+      timestamps: true,
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [{ name: 'id' }],
+        },
+        {
+          name: 'email',
+          unique: true,
+          using: 'BTREE',
+          fields: [{ name: 'email' }],
+        },
+      ],
     },
   );
 
