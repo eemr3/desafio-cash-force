2b9511f230d8d0a86f28d77fc0da1634a24d8f88
eemr3 <eemr3@yahoo.com.br>
2022-12-24 12:44:56 -0300

    style(sideBar): custom activate link

--- web/src/components/SideBar.vue
+++ web/src/components/SideBar.vue
@@ -3,20 +3,20 @@
     <div class="w-full h-[80px] flex items-center ml-4">
       <img class="w-[160px] h-[30px]" src="../assets/logo.svg" alt="logo da aplicação">
     </div>
-    <ul>
-      <li class="flex items-center ml-4">
-        <img src="../assets/icons-nfs.svg" alt="icone NF" class="mr-1">
+    <ul class="p-1">
+      <router-link class="flex items-center w-full border-l-4 border-[#00AD8C]">
+        <img src="../assets/icons-nfs.svg" alt="icone NF" class="ml-1 mr-1">
         Notas fiscais
-      </li>
-      <router-link>Ola</router-link>
+      </router-link>
     </ul>
   </div>
 </template>
 <script>
 export default {
+
   name: "SideBar",
-  data: () => {
-    return { isActive: null }
+  data() {
+    return { active: null }
   }
 }
 </script>
